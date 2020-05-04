9e4cd9cf535b4c236d2fcee761ee0a5988ebd45c
EC2 Default User <ec2-user@ip-172-31-4-239.ap-south-1.compute.internal>
2020-05-04 04:11:35 +0000

    Minor changes

--- src/citiMock.js
+++ src/citiMock.js
@@ -1,25 +1,24 @@
 import queryString from 'query-string';
 import React from 'react';
 import './App.css';
+//import './citiMock.css';
 
 class UserComponent extends React.Component{
     render(){   
-  // url is 'https://www.example.com/user?id=123&type=4';
   const url = this.props.location.search;
   let params = queryString.parse(url);
   console.log(params);
-     let state_parameter = params.version; // have to write fetch the value logic
+     let state_parameter = params.state; 
    let decodedData = Buffer.from(state_parameter, 'base64').toString('ascii');
-   console.log("Decode value for the version is - " + decodedData);
-  // The result will be like below
-  // { id: 123, type: 4 }
-  // other code
+   console.log("********* Decode value ********** = " + decodedData);
+
  return ( 
      <div> 
-     <div><h4><i><em>I am printing the parameters below - </em></i></h4>
-     <h5><i>state = {params.state} </i></h5>
-     <h5><i>date = {params.date} </i></h5>
-     <h5><i>version = {params.version} </i></h5>
+     <div><h4 style={{color: "red"}}><i>I am printing the query parameters along with their values below -</i></h4>
+     <h5><i>code = {params.code} </i></h5>
+     <h5><i>scope = {params.scope} </i></h5>
+     <h5><i>id_token = {params.id_token} </i></h5>
+     <h5><i>state = {decodedData} </i></h5>
 	</div>
 	</div>
      
