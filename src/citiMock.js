ec2a507a845d733daafd6576e9b03880b859880a
EC2 Default User <ec2-user@ip-172-31-5-0.ap-south-1.compute.internal>
2020-05-19 13:46:58 +0000

    fixed broken citi app

--- src/citiMock.js
+++ src/citiMock.js
@@ -1,8 +1,8 @@
 import queryString from 'query-string';
 import React from 'react';
 import Table from 'react-bootstrap/Table';
-//import { URLSearchParams } from 'url';
-//import { URL, URLSearchParams } from 'whatwg-url';
+/*import { URLSearchParams } from 'url';*/
+//import { URLSearchParams } from 'whatwg-url';
 import { Buffer } from 'buffer';
 import './App.css';
 import './citiMock.css';
@@ -11,40 +11,43 @@ class UserComponent extends React.Component{
   
     render(){   
   const url = this.props.location.search;
-  let params = queryString.parse(url);
+  let pparams = queryString.parse(url);
   //const params = new URLSearchParams(url);
-  console.log(params);
-  //if (params.get('state')) {
-     var state_parameter = params.state; 
-   let decodedData = Buffer.from(state_parameter, 'base64').toString('ascii');
+  //console.log(params);
+  console.log(pparams);
+   var state_parameter = pparams.state; 
+   console.log(state_parameter);
+   if (state_parameter!== undefined){
+   var decodedData = Buffer.from(state_parameter, 'base64').toString('ascii');
    console.log("********* Decode value ********** = " + decodedData);
-//  }
+   }
 
- return ( 
+ return (
+  
      <div> 
-     <div><h4 class="container6" style={{color: "red"}}>I am printing the all query parameters with their values in the tabular format below -</h4>
-     <Table striped bordered hover style={{width:"100%"},{align:"center"}}>
+     <div><h4 className="container6" style={{color: "red"}}>Printing all the query parameters with their values in the tabular format below -</h4>
+     <Table striped bordered hover style={{width:"100%"}}>
   <thead>
     <tr>
       <th>Query-Parameter</th>
-      <th>Value</th>
+      <th className ="value">Value</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>code</td>
-      <td>{params.code}</td>
+      <td className="text">code</td>
+      <td>{pparams.code}</td>
     </tr>
     <tr>
-    <td>scope</td>
-      <td>{params.scope}</td>
+    <td className="text">scope</td>
+      <td>{pparams.scope}</td>
     </tr>
     <tr>
-    <td>id_token</td>
-      <td>{params.id_token}</td>
+    <td className="text">id_token</td>
+      <td>{pparams.id_token}</td>
     </tr>
         <tr>
-    <td>state</td>
+    <td className="text">state</td>
       <td>{decodedData}</td>
     </tr>
   </tbody>
